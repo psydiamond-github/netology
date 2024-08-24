baae7cf086244d1d8996bd427afdc89162295178
psydiamond-github <psydiamond@mail.ru>
2024-08-24 19:52:23 +0000

    diplom

---
+++ diplom/variables.tf
@@ -0,0 +1,15 @@
+variable "token_ya" {
+  default = "y0_AgAAAAABC_npAATuwQAAAAD9QKJcAAD7XiLSnHpFm6Dxpvgxow9FN0gTow"
+}
+
+variable "folder_id_ya" {
+  default = "b1g6epuafplhu831ge1h"
+}
+
+variable "cloud_id_ya" {
+  default = "b1geh5m3vpg2dpc0qv06"
+}
+
+variable "image_id_ya" {
+    default = "fd8ba9d5mfvlncknt2kd"
+}
